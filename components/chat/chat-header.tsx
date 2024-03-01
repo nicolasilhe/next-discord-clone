5b9f19fd0470bb1e1992c8803f82de1431b94c68
Nicolas ILHE <nicolas.ilhe@gmail.com>
2024-03-01 12:24:00 +0100

    chat header

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,6 +1,7 @@
 import { Hash } from "lucide-react";
 
 import { MobleToggle } from "@/components/mobile-toggle";
+import { UserAvatar } from "@/components/user-avatar";
 
 interface ChatHeaderProps {
   serverId: string;
@@ -9,12 +10,20 @@ interface ChatHeaderProps {
   imageUrl?: string;
 }
 
-export const ChatHeader = ({ serverId, name, type }: ChatHeaderProps) => {
+export const ChatHeader = ({
+  serverId,
+  name,
+  type,
+  imageUrl,
+}: ChatHeaderProps) => {
   return (
     <div className="text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2">
       <MobleToggle serverId={serverId} />
       {type === "channel" && (
         <Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2" />
+      )}
+      {type === "conversation" && (
+        <UserAvatar src={imageUrl} className="h-8 w-8 md:h-8 md:w-8 mr-2" />
       )}
       <p className="font-semibold text-md text-black dark:text-white">{name}</p>
     </div>
