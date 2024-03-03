a9e563f85aff30ebbdfda62469c61f226cda4be6
Nicolas ILHE <nicolas.ilhe@gmail.com>
2024-03-03 10:10:41 +0100

    fix warnings

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,12 @@ const nextConfig = {
     },
     */
     images: {
-        domains: ['utfs.io']
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "utfs.io",
+            }
+        ]
     }
 };
 
